bc159eb3c533cb51368f183efee970b3a682cfc1
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-03-31 16:11:44 +0700

    migration, database seeder, factory

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        //$todos = Todo::all();
+        $todos = Todo::where('user_id', auth()->user()->id)->get();
+        //colection
+        dd($todos);
+        //array
+        dd($todos->toArray());
+
         return view('todo.index');
+
+        return view('todo.index', compact('todos'));
+
     }
 
     public function create()
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Todo;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +18,29 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'email_verified_at'=> now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' =>Str::random(10),
+            'is_admin'=> true,
+
+        ]);
+
+        User::create([
+            'name' => 'Habib Santoso',
+            'email' => 'habib.santosoti18@gmail.com',
+            'email_verified_at'=> now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' =>Str::random(10),
+            'is_admin'=> false,
+
+        ]);
+
+        User::factory(100)->create();
+        Todo::factory(500)->create();
+
+
     }
 }
