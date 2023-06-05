5ee8f5214a1bc4109c208153f062dbb78ba540e4
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-06-05 14:53:40 +0700

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -9,14 +9,24 @@ class TodoController extends Controller
 {
     public function index()
     {
-        //$todos = Todo::all();
-        $todos = Todo::where('user_id', auth()->user()->id)->get();
-        //colection
-        dd($todos);
-        //array
-        dd($todos->toArray());
+        $todos = Todo::where('user_id', auth()->user()->id)
+        ->orderBy('is_complete','asc')
+        ->orderBy('created_at', 'desc')
+        // ->oderByDesc('created_at')
+        // ->latest()
+        ->get();
 
-        return view('todo.index');
+        // $todos = Todo::where('user_id', auth()->user()->id)
+        //     ->oderBy('is_complete', 'asc')
+        //     ->oderBy('create_at', 'desc')
+        //     ->get();
+
+        //colection
+        // dd($todos);
+        //array
+        // dd($todos->toArray());
+
+        // return view('todo.index');
 
         return view('todo.index', compact('todos'));
 
@@ -25,6 +35,34 @@ class TodoController extends Controller
     public function create()
     {
         return view('todo.create');
+    }
+
+    public function store(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        // Practical
+        // $todo = new Todo;
+        // $todo->title = $request->title;
+        // $todo->user_id = auth()->user()->id;
+        // $todo->save();
+
+        // Query Builder way
+        // DB::table('todos')->insert([
+        //     'title' => $request->title,
+        //     'user_id' => auth()->user()->id,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
+
+        // Eloquent Way - Readable
+        $todo = Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
     }
 
     public function edit()
